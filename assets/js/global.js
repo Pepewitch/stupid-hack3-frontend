976f02a796fe904dfda91285cb4fe890e23a337f
pepe_witch <skpepe134@gmail.com>
2019-04-21 05:35:12 +0700

    :sparkles: updatet landing page

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,3 +1,31 @@
 $(document).ready(function() {
-    console.log('hello world')
-})
+  const $coverBottom = $("#cover-bottom");
+  const $file = $("#file");
+  $file.change(function(e) {
+    const file = e.target.files[0];
+    if (file && file.type === "text/plain") {
+      console.log("get file");
+    } else {
+        alert('Please drop text file')
+    }
+  });
+  $coverBottom.click(function() {
+    $file.click();
+  });
+  const coverBottom = document.getElementById("cover-bottom-image");
+  coverBottom.ondragenter = () =>
+    setCoverBottomImage("/assets/image/landing2-drop.png");
+  coverBottom.ondragleave = () =>
+    setCoverBottomImage("/assets/image/landing2.png");
+  coverBottom.ondragend = e => e.preventDefault();
+  coverBottom.ondragover = e => e.preventDefault();
+  coverBottom.ondrop = e => dropFile(e);
+});
+function dropFile(e) {
+  e.preventDefault();
+  console.log(e.dataTransfer.items);
+  setCoverBottomImage("/assets/image/landing2.png");
+}
+function setCoverBottomImage(src) {
+  $("#cover-bottom-image").attr("src", src);
+}
